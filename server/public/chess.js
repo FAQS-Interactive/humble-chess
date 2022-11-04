33e2922fe8851df9a48af94840a8d173a49392aa
AL Lin <alqianhe7@gmail.com>
2022-11-04 17:32:14 +1300

    Find all of the pawns with a filter method

--- server/public/chess.js
+++ server/public/chess.js
@@ -1,7 +1,27 @@
-const pieces = document.getElementById('piece')
 const tableCells = document.getElementsByTagName('td')
+const tableArr = [...tableCells]
 
-const arr = [...tableCells]
+// let pawnPiece = document.createElement('img')
 
-console.log(pieces)
-console.log(arr)
+function pawn() {
+  let img = document.createElement('img')
+  img.src = './images/pawn.png'
+  img.alt = 'pawn piece'
+  img.id = 'pawn'
+
+  tableArr.map((x, i) =>
+    i > 47 && i < 56 ? x.appendChild(img.cloneNode(true)) : null
+  )
+
+  // console.log(test)
+  console.log(tableArr)
+}
+
+pawn()
+
+const occupiedCells = tableArr.filter(
+  (x) =>
+    // let cells = document.getElementsByTagName('td')
+    x.innerHTML
+)
+console.log(occupiedCells)
